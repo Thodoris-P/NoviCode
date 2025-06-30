7640d2c48a29c05a33f569f4b2356ac70bf19f65
Thodoris-P <thodoris99@hotmail.com>
2025-06-30 21:02:26 +0300

    add migrations

---
+++ NoviCode.Data/Migrations/20250629142719_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoviCode.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    EffectiveAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => new { x.Currency, x.EffectiveAt });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wallets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Balance = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+
+            migrationBuilder.DropTable(
+                name: "Wallets");
+        }
+    }
+}
---
+++ NoviCode.Data/Migrations/20250630163335_AddExchangeRateType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoviCode.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExchangeRateType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE TYPE dbo.ExchangeRateType AS TABLE
+            (
+                Currency    NVARCHAR(3)   NOT NULL,
+                EffectiveAt DATETIME2     NOT NULL,
+                Rate        DECIMAL(18,6) NOT NULL
+            );
+        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TYPE IF EXISTS dbo.ExchangeRateType;");
+        }
+    }
+}
